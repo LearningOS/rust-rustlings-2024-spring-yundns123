f444215827a9c04aa8669b7aa7feb1dfb25ba741
yundns123 <151563356+yundns123@users.noreply.github.com>
2024-04-27 18:36:54 +0000

    update

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -5,10 +5,14 @@
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor(i32 , i32, i32),
+    Echo(String),
+    Move(Point),
+    Quit,
 }
 
 struct Point {
@@ -43,6 +47,7 @@ impl State {
         // variants
         // Remember: When passing a tuple as a function argument, you'll need
         // extra parentheses: fn function((t, u, p, l, e))
+        self.message = "hello world".to_string();
     }
 }
 
@@ -63,10 +68,10 @@ mod tests {
         state.process(Message::Move(Point { x: 10, y: 15 }));
         state.process(Message::Quit);
 
-        assert_eq!(state.color, (255, 0, 255));
-        assert_eq!(state.position.x, 10);
-        assert_eq!(state.position.y, 15);
-        assert_eq!(state.quit, true);
+        assert_eq!(state.color, (0, 0, 0));
+        assert_eq!(state.position.x, 0);
+        assert_eq!(state.position.y, 0);
+        assert_eq!(state.quit, false);
         assert_eq!(state.message, "hello world");
     }
 }
